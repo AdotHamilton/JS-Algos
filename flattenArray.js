c3cd7f2304b4cb8e97503b635e3f3f0d6d4ccfba
Austin Hamilton <austin.hamilton.j@gmail.com>
2021-04-08 09:37:59 -0500

    Initial commit

---
+++ flattenArray.js
@@ -0,0 +1,22 @@
+const flatten = (arr) => {
+    for(let i= 0; i < arr.length; i++){
+        if(typeof arr[i] === 'object'){
+            if(arr[i].length === 0){
+                console.log('[] found')
+                arr.splice(i, 1);
+            } else {
+                let temp = [];
+                for(let j=0; j< arr[i].length; j++){
+                    temp.push(arr[i][j]);
+                }
+                arr.splice(i, 1)
+                let arr2 = arr.splice(i)
+                arr = arr.concat(temp,arr2)
+            }
+            
+        }
+    }
+    return(arr)
+};
+
+console.log(flatten([1,2,[3,[4, 5]], 5, 6, []]))
